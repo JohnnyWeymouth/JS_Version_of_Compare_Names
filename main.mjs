b010b20421c161d7873069b962eb49ec44fffc29
JohnnyWeymouth <johnnycweymouth@gmail.com>
2023-06-10 19:50:16 -0700

    finished testing

---
+++ main.mjs
@@ -0,0 +1,5 @@
+import { compareTwoNames } from './compareNames.mjs';
+
+// Call the function and update the element's content
+let result = compareTwoNames("Johnny C Weymouth", "weymouth john christian");
+console.log(result);
--- testing/main.mjs
+++
@@ -1,7 +0,0 @@
-import { compareTwoNames } from '../compareNames.mjs';
-
-// Get the HTML element
-const resultElement = document.getElementById('result');
-
-// Call the function and update the element's content
-resultElement.innerHTML = compareTwoNames("Johnny C Weymouth", "John weymouth christian");
